3eb23bfbcd6179478ac6393cf2f8eedb8f435032
Donia102950 <donia1995mohamed@gmail.com>
2020-03-31 22:46:52 +0200

    Adding the libraries 1)BIT_MATH.h 2)STD_TYPES.h Signed-off-by: Donia102950 <donia1995mohamed@gmail.com>

---
+++ COTS/Libraries/STD_TYPES2.h
@@ -0,0 +1,14 @@
+typedef unsigned char uint_8t;
+typedef unsigned short int uint_16t;
+typedef unsigned long int uint_32t;
+typedef unsigned long long uint_64t;
+typedef signed char int_8t;
+typedef signed short int int_16t;
+typedef signed short int int_32t;
+
+#define OK       0
+#define NOT_OK   1
+
+
+#define NULL ( (void *) 0)
+
